ea2542a5ce7cdf65a28a9c438fa6ab8aa2f27df5
minhmanitobss09@gmail.com <50633992+Sundayyy@users.noreply.github.com>
2020-03-24 16:11:47 +0700

    Updates

---
+++ precache-manifest.818543d1e7c9cb28102272176f939162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c50fc8d074e3e9303a0e4bcbfca75a",
+    "url": "/React-example/index.html"
+  },
+  {
+    "revision": "e52913dc7c3c9c0b0652",
+    "url": "/React-example/static/css/main.a23ec21c.chunk.css"
+  },
+  {
+    "revision": "abaf2c1f2dcd6506c504",
+    "url": "/React-example/static/js/2.4e04b72a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-example/static/js/2.4e04b72a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52913dc7c3c9c0b0652",
+    "url": "/React-example/static/js/main.2d7c3e82.chunk.js"
+  },
+  {
+    "revision": "f4f201766264ba9d7d62",
+    "url": "/React-example/static/js/runtime-main.b495bae0.js"
+  }
+]);
